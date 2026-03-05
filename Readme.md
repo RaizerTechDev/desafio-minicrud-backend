08d473a63ef7395fe316e564649e749ccea07d00
RafaRz76Dev <rafarz76dev@gmail.com>
2026-03-05 01:15:22 -0300

    :tada: chore: initial commit

---
+++ Readme.md
@@ -0,0 +1,119 @@
+# 📦 API CRUD de Produtos
+
+API Backend desenvolvida com **Node.js** e **Express** para demonstrar as operações básicas de **CRUD** (Create, Read, Update, Delete).
+
+---
+
+## *📖 Sobre o Projeto
+
+Esta aplicação é uma API REST Backend responsável por gerenciar produtos através de endpoints REST.
+
+Cada produto possui os seguintes atributos:
+
+* **id** → Identificador único do produto
+* **nome** → Nome do produto
+* **preco** → Valor do produto
+
+A aplicação utiliza um **array em memória** para simular um banco de dados.
+
+---
+
+## **🧰 Tecnologias Utilizadas**
+
+* Node.js
+* Express.js
+* Postman
+
+---
+
+## **📚 Documentação da API**
+
+A documentação completa da API pode ser acessada no link abaixo:
+
+***🔗 Documentação no Postman***
+
+https://documenter.getpostman.com/view/19569624/2sBXcLec9Y
+
+---
+
+## **⚙️ Como Executar o Projeto**
+
+### ***1️⃣ Clonar o repositório***
+
+```bash
+git clone https://github.com/seu-usuario/seu-repositorio.git
+```
+ou 
+
+### ***📂 Criar pasta***
+
+```bash
+npm init -y
+```
+---
+
+### ***2️⃣ Instalar dependências***
+
+```bash
+npm install express
+npm install nodemon --save-dev   
+```
+
+---
+
+### ***3️⃣ Executar o servidor***
+
+```bash
+npm run dev
+```
+
+ou
+
+```bash
+node server.js
+```
+
+---
+
+## **🚀 Endpoints da API**
+
+### - POST /produtos => ***Criar Produto***
+
+### - GET /produtos => ***Listar Produtos***
+
+### - GET /produtos/:id => ***Listar Produtos por {ID}***
+
+### - PUT /produtos/:id => ***Atualizar Produtos por {ID}***
+
+### - DELETE /produtos/:id => ***Remover Produtos por {ID}***
+
+---
+
+## **📌 Status HTTP Utilizados**
+
+* **200** → Sucesso
+* **201** → Recurso criado
+* **204** → Removido com sucesso
+* **404** → Recurso não encontrado
+
+---
+
+## **👨‍💻 Autor**
+
+Projeto desenvolvido para prática de **CRUD com API REST usando Express**.
+
+<table>
+<tr>
+  <td align="center">
+    <img src="https://avatars.githubusercontent.com/u/87991807?v=4" width="80" />
+  </td>
+  <td>
+    **RafaRaizer-Dev** <br>
+    <a href="https://api.whatsapp.com/send/?phone=47999327137">📱 WhatsApp</a> | 
+    <a href="https://www.linkedin.com/in/raizer-rafael/">💼 LinkedIn</a> | 
+    <a href="https://github.com/RaizerTechDev">🐱 GitHub</a> | 
+    <a href="https://raizertechdev-portfolio.netlify.app/">🌐 Portfólio</a>
+  </td>
+</tr>
+</table>
+
---
+++ server.js
@@ -0,0 +1,105 @@
+const express = require("express") // * Importa o Express
+const app = express() // * Cria a aplicação
+const PORT = 3000  // * Rodar na porta 3000
+
+app.use(express.json()) // * Permite ler JSON no body das requisições
+
+// * Banco de Dados"* Nova variavel para gerar o ID automático fake (array em memória)
+let produtos = [
+  {
+    id: 1,  
+    nome: "Mouse",
+    preco: 65.00
+  },  
+    {
+    id: 2,    
+    nome: "Teclado",
+    preco: 120.00
+  }    
+]
+
+// * Rota Principal
+app.get("/", (req, res) => {
+  res.send("Sevidor rodando com Express!")
+})
+
+// * CREATE → Usando push() para criar o produto + req.body para Dados Enviados
+app.post("/produtos", (req, res) => {
+  const {nome, preco} = req.body
+  
+  // Criar nova variável
+  const novoProduto = {
+    id: produtos.length + 1, // * Para gerar ID automático simples
+    nome,
+    preco,
+  }
+
+  // * Adicionar o novo produto no array com push 
+  produtos.push(novoProduto) 
+
+  // * Retorna o produto criado
+  res.status(201).json(novoProduto)  
+})
+
+// * READ → Listar produtos 
+app.get("/produtos", (req, res) => {
+  res.status(200).json(produtos)
+})
+
+// * READ → Listar produtos com req.params para capturar ID e por ID find() para buscar o produto pelo ID
+app.get("/produtos/:id", (req, res) => {
+  const { id } = req.params // * Captura o ID da Rota
+  
+  const produto = produtos.find(p => p.id == id) // * Busca produto pelo ID
+
+  // * Se não existir
+  if(!produto) {
+    return res.status(400).json({ mensagem: "Produto não encontrado!"})
+  }
+
+  // * Retorna o produto listado por ID
+  res.status(200).json(produto)
+})
+
+// * UPDATE → Atualizar produto com req.params para capturar ID , req.body para Pegar Novos Dados e por ID find() para buscar o produto pelo ID
+app.put("/produtos/:id", (req, res) => {
+  const { id } = req.params // * Captura o ID da Rota
+  const {nome, preco} = req.body // * para Pegar Novos Dados
+
+  const produto = produtos.find(p => p.id == id) // * Buscar Produto pelo ID
+
+  // * Se não existir
+  if(!produto) {
+    return res.status(404).json({ mensagem: "Produto não encontrado!"})
+  }
+
+  // * Aqui atualiza nome e preço
+  produto.nome = nome 
+  produto.preco = preco 
+
+  // * Retorna o produto listado por ID
+  res.status(200).json(produto)
+})
+
+// * DELETE - Remover Produto com req.params para capturar ID, find() para buscar o produto pelo ID e filter para remover o produto
+app.delete("/produtos/:id", (req, res) => {
+  const { id } = req.params  // * Captura o ID da Rota
+
+  const produto = produtos.find(p => p.id == id) // * Buscar Produto pelo ID
+
+  // * Se não existir
+  if(!produto) {
+    return res.status(404).json({ mensagem: "Produto não encontrado!"})  
+}
+
+  // * Remove o produto criando um novo array sem ele
+  produtos = produtos.filter(p => p.id != id)
+
+ // * Retorna No Content
+  res.status(204).send()
+})
+
+// * listen vai ouvir a porta 3000 e rodar o servidor no terminal
+app.listen(PORT, () =>{
+  console.log(`Servidor rodando em http://localhost:${PORT}`)
+})
